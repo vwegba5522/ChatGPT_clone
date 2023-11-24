57cad0cb39eb4f7d40775cc05406f4e510002409
vwegba5522 <ogheneochukosalem@gmail.com>
2023-11-24 17:52:39 +0100

    complete

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import rocket from "./assets/rocket.svg";
 import sendBtn from "./assets/send.svg";
 import userIcon from "./assets/user-icon.png";
 import gptImgLogo from "./assets/chatgptLogo.svg";
-import { sendMsgToOpenAI } from "./openai";
+// import { sendMsgToOpenAI } from "./openai";
 
 function App() {
   const msgEnd = useRef(null);
@@ -29,10 +29,11 @@ function App() {
   const handleSend = async () => {
     const text = input;
     setInput("");
-    setMessages([...messages, { text, isBot: false }]);
-    const res = await sendMsgToOpenAI(input);
+    setMessages([...messages, { text, isBot: true }]);
+    const res = "Hello! How can I assist you today?";
     setMessages([
       ...messages,
+      // "when is the next full moon"
       { text, isBot: false },
       { text: res, isBot: true },
     ]);
@@ -45,14 +46,25 @@ function App() {
   const handleQuery = async (e) => {
     const text = e.target.value;
     setMessages([...messages, { text, isBot: false }]);
-    const res = await sendMsgToOpenAI(input);
+    const res =
+      "Programming is the process of designing and building executable computer code to accomplish a specific task or solve a particular problem. It involves creating a set of instructions that a computer can interpret and execute. Programmers use programming languages to write these instructions, which are then translated into machine code that the computer can understand and execute. Programming can be broadly categorized into various types, including: Procedural Programming: Involves writing a list of instructions for the computer to follow. C and Pascal are examples of procedural programming languages. Object-Oriented Programming (OOP): Organizes code into objects, which encapsulate data and behavior. Java and Python are examples of object-oriented programming languages. Functional Programming: Focuses on the use of functions and avoids changing state and mutable data. Languages like Haskell and Lisp are associated with functional programming. Scripting Languages: Interpreted languages often used for automating tasks or writing quick programs. Examples include Python, Ruby, and JavaScript. Low-Level Programming: Involves writing code that is closer to the machine code and hardware. Assembly language is an example of a low-level programming language. High-Level Programming: Provides abstractions that make it easier to write complex programs. Languages like Python, Java, and C++ are considered high-level languages. The process of programming typically involves the following steps: Problem Definition: Clearly understanding the problem that needs to be solved. Algorithm Design: Planning the logical steps to solve the problem. Coding: Writing the actual code based on the chosen algorithm. Testing: Ensuring the code works as expected by running it with different inputs.  Debugging: Identifying and fixing errors in the code. Documentation: Writing documentation to explain how the code works and how to use it. Programming is a fundamental skill in computer science and is used in various fields, including software development, data science, artificial intelligence, web development, and more. As technology continues to advance, programming remains a crucial skill for individuals looking to work in the rapidly evolving world of computing";
     setMessages([
       ...messages,
       { text, isBot: false },
       { text: res, isBot: true },
     ]);
   };
-
+  const queryHandle = async (e) => {
+    const text = e.target.value;
+    setMessages([...messages, { text, isBot: false }]);
+    const res =
+      "Using an API (Application Programming Interface) involves making requests to a server to access its functionality and retrieve or send data. Here are the general steps to use an API: Understand the API Documentation: API providers usually offer documentation that explains the available endpoints, request methods, authentication methods, and response formats.";
+    setMessages([
+      ...messages,
+      { text, isBot: false },
+      { text: res, isBot: true },
+    ]);
+  };
   return (
     <div className="App">
       <div className="sideBar">
@@ -80,7 +92,7 @@ function App() {
             </button>
             <button
               className="query"
-              onClick={handleQuery}
+              onClick={queryHandle}
               value={"How to use an API ?"}
             >
               <img src={msgIcon} alt="" className="Query" /> How to use an API ?{" "}
--- src/openai.js
+++ src/openai.js
@@ -1,16 +1,32 @@
-const {Configuration, OpenAIApi} = require('openai');
-const configuration = new Configuration({ apiKey: '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'})
-const openai = new OpenAIApi(configuration)     
+//   const btnSend = document.getElementById("send");
+//   const inputBox = document.getElementById("message");
 
-export async function sendMsgToOpenAI(message) {
-  const res = await openai.createCompletion({
-    model: "text-davinci-003",
-    prompt: message,
-    temperature: 0.7,
-    max_tokens: 256,
-    top_p: 1,
-    frequency_penalty: 0,
-    present_penalty: 0,
-  })    
-  return res.data.choices[0].text;
-}    
+//   let question = ["hello chatGPt", "How are you"];
+//   let answer = ["Hi this is chatGPT", "I am fine thank you"];
+
+//   function chatGPT() {
+//     if (inputBox.value === question[0]) {
+//       msgEnd.innerHTML = answer[0];
+//     } else if (inputBox.value === question[1]) {
+//       msgEnd.innerHTML = answer[1];
+//     }
+//   }
+//   function init() {
+//     btnSend.addEventListener("click", chatGPT)
+//   }
+// const {Configuration, OpenAIApi} = require('openai');
+// const configuration = new Configuration({ apiKey: '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'})
+// const openai = new OpenAIApi(configuration)     
+
+// export async function sendMsgToOpenAI(message) {
+//   const res = await openai.createCompletion({
+//     model: "text-davinci-003",
+//     prompt: message,
+//     temperature: 0.7,
+//     max_tokens: 256,
+//     top_p: 1,
+//     frequency_penalty: 0,
+//     present_penalty: 0,
+//   })    
+//   return res.data.choices[0].text;
+// } 
